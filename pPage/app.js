de2cf5161c970a25f35de8bcc92d641e08882bc6
yuubd <yuubd@github.com>
2017-12-21 17:08:31 -0800

    practice dec 20~21

--- pPage/app.js
+++ pPage/app.js
@@ -1,2 +1,45 @@
+const port = 8080;
+const _ = require('underscore');
 var express = require('express');
 var app = express();
+app.locals.pretty = true; // makes pug looks pretty
+
+app.use(express.static('public'));              // access to files
+app.listen(port, ()=>{
+	console.log(`Connected to ${port} port`);
+});
+
+app.set('view engine', 'pug');
+app.set('views', './contents')
+
+
+app.get('/', (req, res)=>{
+	res.render('layout');
+	var pic =`<img src="/pic.png" id="frontPic">`;
+	//var fpic = document.get.ElementById("frontPic");
+	//fpic.style.width = "200";
+	//fpic.style.height = "200";
+	// var intro = `Hello David\'s home page`;
+	// var topics = [
+	// 	'Education: UBC',
+	// 	'Work Experience: a lot',
+	// 	'Projects: one or two',
+	// 	'Languages: ENG, KOR, and SPAN',
+	// 	'Extracurriculum: some',
+	// 	'Volunteer: a few'
+	// ];
+	// var list = '';
+	// for(var i=0; i<_.size(topics); i++){
+	// 	list += `<li>${topics[i]}</li>`;
+	// }
+	// var output =`                   
+	// 	<a href="/?id=0">- Education</a><br>
+	// 	<a href="/?id=1">- Work Experience</a><br>
+	// 	<a href="/?id=2">_Projects</a><br>
+	// 	<a href="/?id=3">_Languages</a><br>
+	// 	<a href="/?id=4">_Extracurriculum</a><br>
+	// 	<a href="/?id=5">_Volunteer</a><br>
+	// `;
+	// //${topics[req.query.id]}
+	// res.send(intro +`<br>`+ output +`<br>`+ list);        // renders any number after /topic?id=
+});
--- sever_side_javascript/app.js
+++ sever_side_javascript/app.js
@@ -1,11 +1,46 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');  // brought body-parser module           
 app.locals.pretty = true; // makes pug looks pretty
 app.use(express.static('public')); // setting a directory where all the static files locate
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.set('view engine', 'pug');     // specifying the engine
 app.set('views', './views');       // tradition that pug requires views dir
+app.get('/login', (req, res) => { 
+	res.send('<h1>No need to login! Go back to Home</h1>');
+});
 
-app.get('/topic', (req, res)=>{    // /topic?id=
+app.listen(3000, function(){  // assign a port number
+	console.log('Connected 3000 Port!');
+});
+
+app.get('/index', function(req, res) {
+	res.render('index', {message2:'David\'s Page', message: 'Hi there!', time: Date()})
+});
+
+app.get('/', (req, res) => {  // '/' == home  // through URL == get  // use function to displaty whatever I want to show
+	var pic = `<img src="/pic.png" id='fpic'>`;
+	//var myimg = document.getElementById('fpic');
+	res.send(`Hello David\'s home page ${pic}`);
+});
+
+app.get('/form', (req,res)=>{     // build a form
+	res.render('form');
+});
+
+app.post('/form_receiver', (req,res)=>{  // it saves the info from form(action='/form_receiver')
+	var title = req.body.title;          // get=query, post=body
+	var description = req.body.description;
+	res.send(title +', '+ description);
+});
+
+app.get('/form_receiver', (req,res)=>{  // it saves the info from form(action='/form_receiver')
+	var title = req.query.title;
+	var description = req.query.description;
+	res.send(title +', '+ description);
+});
+
+app.get('/topic/:id', (req, res)=>{    // /topic?id=     **'/topic/:id'
 	var topics = [                 // can be replaced by db
 		'Languages: ENG, KOR, and SPAN',
 		'Education: UBC',
@@ -14,21 +49,18 @@ app.get('/topic', (req, res)=>{    // /topic?id=
 		'Volunteer: a few'
 	];
 	var output =`                   
-		<a href="/topic?id=0">Languages</a><br>
-		<a href="/topic?id=1">Education</a><br>
-		<a href="/topic?id=2">Work Experience</a><br>
-		<a href="/topic?id=3">Extracurriculum</a><br>
-		<a href="/topic?id=4">Volunteer</a><br>
-		${topics[req.query.id]}
-	`;
-
+		<a href="/topic/0">Languages</a><br>
+		<a href="/topic/1">Education</a><br>
+		<a href="/topic/2">Work Experience</a><br>
+		<a href="/topic/3">Extracurriculum</a><br>
+		<a href="/topic/4">Volunteer</a><br>
+		${topics[req.params.id]}   
+	`;      // topics[req.params.id] if '/topic/:id'
+			// topics[req.query.id] if  '/topic'
 	res.send(output);        // renders any number after /topic?id=
 })
 
 
-app.get('/', (req, res) => {  // '/' == home  // through URL == get  // use function to displaty whatever I want to show
-	res.send('Hello David\'s home page <img src="/pic.png">');
-});
 app.get('/dynamic', function(req, res){
 	var list = '';
 	for(var i=0; i<5; i++){
@@ -52,17 +84,6 @@ app.get('/dynamic', function(req, res){
 	`;
 	res.send(output);
 
-});
-app.get('/login', (req, res) => { 
-	res.send('<h1>No need to login! Go back to Home</h1>');
-});
-
-app.listen(3000, function(){  // assign a port number
-	console.log('Connected 3000 Port!');
-});
-
-app.get('/index', function(req, res) {
-	res.render('index', {title:'David\' Page', message: 'Hi there!', time: Date()})
 });
 
 
